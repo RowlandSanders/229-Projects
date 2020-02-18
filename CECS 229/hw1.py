724a2ef686038f3d7d22cf06cd4a5256a74c23aa
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-02-17 23:19:33 -0800

    python update 1

--- CECS 229/hw1.py
+++ CECS 229/hw1.py
@@ -15,10 +15,54 @@
 # Submit the following Python functions in one Jupyter Notebook after demoing your work.
 # 
 # 
+# #### Problem 1:
+# 
+# Program a function `modExp(b, n, m)` that computes $$b^n \mod m$$ using the algorithm discussed in lecture.  The function should satisfy the following:
+# 
+# 1. INPUT: 
+#     * `b` - positive integer representing the base
+#     * `n` - positive integer representing the exponent
+#     * `m` - positive integer representing the modulo
+# 
+#     
+# 2. OUTPUT:
+#     * the computation of $b^n \mod m$
+# 
+#  
+# EXAMPLE: 
+# 
+# `>> modExp( 3 , 644, 645 )`
+# 
+# `36`
+# 
+# 
+
+# In[1]:
+
 
 def modExp(b, n, m):
     sum=(b**n)%m
     return sum
+
+
+# #### Problem 2:
+# 
+# Create a function `bezoutCoeffs(a, b)` that computes the Bezout coefficients `s` and `t` of `a` and `b`.
+# 
+# 1. INPUT: 
+#     * `a`,`b` - integers
+# 
+# 2. OUTPUT: `(s, t)` - tuple of coefficients
+# 
+# EXAMPLE:  
+#  
+# `>> bezoutCoeffs(414, 662)` 
+# 
+# `(8, -5)`
+# 
+# 
+
+# In[2]:
 
 
 def bezoutCoeffs(a, b):
@@ -41,6 +85,25 @@ def bezoutCoeffs(a, b):
         
     return (x1,y1)
 
+
+# #### Problem 3:
+# 
+# Create a function `gcd(a, b)` that computes the greatest common divisor of `a` and `b`
+# 
+# 1. INPUT: 
+#     * `a`,`b` - integers
+# 
+# 2. OUTPUT: `d` - the gcd
+# 
+# EXAMPLE:  
+#  
+# `>> gcd(414, 662)` 
+# 
+# `2`
+
+# In[3]:
+
+
 def gcd(a,b):
     while b: 
        a, b = b, a % b 
@@ -48,10 +111,24 @@ def gcd(a,b):
     return sum
 
 
+# In[5]:
+
 
 """ TESTER CELL """
 print("Testing modExp(23, 1002, 41) = ", modExp(23, 1002, 41))
 print("Testing bezoutCoeffs(26, 7) = ", bezoutCoeffs(26, 7))
 print("Testing gcd(101, 4620) = ", gcd(101, 4620))
 print("Testing gcd(2349, 36) = ", gcd(2349, 36))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 

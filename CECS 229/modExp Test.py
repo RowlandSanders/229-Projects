f764ed096256fe50b2cfbf0bd04efcaba87a8d47
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-03-10 12:18:36 -0700

    Update and Test

--- CECS 229/modExp Test.py
+++ CECS 229/modExp Test.py
@@ -4,16 +4,9 @@ Spyder Editor
 
 This is a temporary script file.
 """
-def modExp(b, n, m) :      
-    
-    b=b%m  
-    sum=0
-    t = 1
-    while (n > 0) : 
-        if ((n & 1)==1) : 
-            t=(t*b)%m
-        b = (b**2)%m 
-        n = n >> 1 #bitwise shift to right
-    sum = t
-    
-    return sum
+def modInverse(a, m) : 
+    a = a % m; 
+    for x in range(1, m) : 
+        if ((a * x) % m == 1) : 
+            return x 
+    return 1

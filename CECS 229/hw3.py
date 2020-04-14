3556eb1e61e2802993e7d0f7396fec16eee7681b
Rowland Sanders <48600494+RowlandSanders@users.noreply.github.com>
2020-04-14 11:54:31 -0700

    Update hw3.py

--- CECS 229/hw3.py
+++ CECS 229/hw3.py
@@ -1,19 +1,20 @@
-import math
 class Vec:
     def __init__(self, contents = []):
         self.elements = contents
-
+    
     def __abs__(self):
         out = 0
-        for i in self.elements:
-            out += i ** 2
-        out = math.sqrt(out)
         
-        return out 
+        for i in self.elements:
+            out += i**2
+        out = out ** 0.5
+        
+        return out
         pass
-
+        
     def __add__(self, other):
-        oAdd = Vec([])
+        
+        oAdd= Vec([])
         if len(self.elements) != len(other.elements):
             raise ValueError
             
@@ -27,48 +28,43 @@ class Vec:
     
     def __mul__(self, other):
         if type(other) == Vec: 
-            
-            if len(self.elements)!=len(other.elements):
+            if len(self.elements) != len(other.elements):
                 raise ValueError
                 
             else:
                 out = 0
                 for i in range(len(self.elements)):
                     out = out + (self.elements[i] * other.elements[i])
-                    
             return out
             pass
-        
         elif type(other) == float or type(other) == int: 
-            
             out = Vec([])
-            
             for i in self.elements:
-                out.elements.append(int(i * other))
-                
+                out.elements.append(i * other)
             return out
             pass
-
-
+            
+    
     def __rmul__(self, other):
-        out = Vec([])
+
+        out=Vec([])
         for i in self.elements:
-            out.elements.append(int(i * other))
+            out.elements.append(i * other)
             
         return out
         pass
-
+    
     def __str__(self):
         return str(self.elements) 
     
     def __sub__(self, other):
-      
         oAdd = Vec([])
-        
         if len(self.elements) != len(other.elements):
             raise ValueError
+            
         else:
             for i in range(len(self.elements)):
                 oAdd.elements.append(self.elements[i] - other.elements[i])
+                
         return oAdd
         pass
